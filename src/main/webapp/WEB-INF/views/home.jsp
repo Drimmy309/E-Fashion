63bf4f4ae9f981fb38811dda431d664f2cae87c1
jim <qatrinha@gmail.com>
2026-02-08 12:23:58 +0700

    initial

---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>E-Fashion</title>
+</head>
+<body>
+    <h1>JSP hoạt động thành công</h1>
+</body>
+</html>
---
+++ src/test/java/com/example/efashion/EFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
